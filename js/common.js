86bab894514b3a1d09426ab6818eba4a617daa1a
小松菜奈 <11910223+nana-komatsuu@user.noreply.gitee.com>
2023-02-01 16:02:40 +0800

    完成了拦截器

--- js/common.js
+++ js/common.js
@@ -12,3 +12,28 @@ function tip(msg) {
   toastBody.innerHTML = msg;
   myToast.show();
 }
+axios.defaults.baseURL = "http://ajax-api.itheima.net";
+axios.interceptors.request.use(
+  function (config) {
+    // 在发送请求之前做些什么
+    config.headers.authorization = localStorage.getItem("token");
+    return config;
+  },
+  function (error) {
+    // 对请求错误做些什么
+    return Promise.reject(error);
+  }
+);
+
+axios.interceptors.response.use(
+  function (response) {
+    // 2xx 范围内的状态码都会触发该函数。
+    // 对响应数据做点什么
+    return response.data;
+  },
+  function (error) {
+    // 超出 2xx 范围的状态码都会触发该函数。
+    // 对响应错误做点什么
+    return Promise.reject(error);
+  }
+);
