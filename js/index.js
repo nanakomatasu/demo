99bf462fcd1ef7093a87cf8d6dd205ccacc51719
nanakomatasu <123913335+nanakomatasu@users.noreply.github.com>
2023-02-02 21:26:36 +0800

    新增了地图

--- js/index.js
+++ js/index.js
@@ -307,6 +307,115 @@ async function getBack() {
     myChart.setOption(option);
   }
   salaryDouble(salaryArr);
+  //地图
+  const initMapChart = (provinceData) => {
+    const myEchart = echarts.init(document.querySelector("#map"));
+    const dataList = [
+      { name: "南海诸岛", value: 0 },
+      { name: "北京", value: 0 },
+      { name: "天津", value: 0 },
+      { name: "上海", value: 0 },
+      { name: "重庆", value: 0 },
+      { name: "河北", value: 0 },
+      { name: "河南", value: 0 },
+      { name: "云南", value: 0 },
+      { name: "辽宁", value: 0 },
+      { name: "黑龙江", value: 0 },
+      { name: "湖南", value: 0 },
+      { name: "安徽", value: 0 },
+      { name: "山东", value: 0 },
+      { name: "新疆", value: 0 },
+      { name: "江苏", value: 0 },
+      { name: "浙江", value: 0 },
+      { name: "江西", value: 0 },
+      { name: "湖北", value: 0 },
+      { name: "广西", value: 0 },
+      { name: "甘肃", value: 0 },
+      { name: "山西", value: 0 },
+      { name: "内蒙古", value: 0 },
+      { name: "陕西", value: 0 },
+      { name: "吉林", value: 0 },
+      { name: "福建", value: 0 },
+      { name: "贵州", value: 0 },
+      { name: "广东", value: 0 },
+      { name: "青海", value: 0 },
+      { name: "西藏", value: 0 },
+      { name: "四川", value: 0 },
+      { name: "宁夏", value: 0 },
+      { name: "海南", value: 0 },
+      { name: "台湾", value: 0 },
+      { name: "香港", value: 0 },
+      { name: "澳门", value: 0 },
+    ];
+    let option = {
+      title: {
+        text: "籍贯分布",
+        top: 10,
+        left: 10,
+        textStyle: {
+          fontSize: 16,
+        },
+      },
+      tooltip: {
+        trigger: "item",
+        formatter: "{b}: {c} 位学员",
+        borderColor: "transparent",
+        backgroundColor: "rgba(0,0,0,0.5)",
+        textStyle: {
+          color: "#fff",
+        },
+      },
+      visualMap: {
+        min: 0,
+        max: 6,
+        left: "left",
+        bottom: "20",
+        text: ["6", "0"],
+        inRange: {
+          color: ["#ffffff", "#0075F0"],
+        },
+        show: true,
+        left: 40,
+      },
+      geo: {
+        map: "china",
+        roam: false,
+        zoom: 1.0,
+        label: {
+          normal: {
+            show: true,
+            fontSize: "10",
+            color: "rgba(0,0,0,0.7)",
+          },
+        },
+        itemStyle: {
+          normal: {
+            borderColor: "rgba(0, 0, 0, 0.2)",
+            color: "#e0ffff",
+          },
+          emphasis: {
+            areaColor: "#34D39A",
+            shadowOffsetX: 0,
+            shadowOffsetY: 0,
+            shadowBlur: 20,
+            borderWidth: 0,
+            shadowColor: "rgba(0, 0, 0, 0.5)",
+          },
+        },
+      },
+      series: [
+        {
+          name: "籍贯分布",
+          type: "map",
+          geoIndex: 0,
+          data: dataList,
+        },
+      ],
+    };
+    myEchart.setOption(option);
+  };
+  initMapChart();
+  //地图
   for (const k in arr) {
     document.querySelector(`[name=${k}]`).innerHTML = arr[k];
   }
