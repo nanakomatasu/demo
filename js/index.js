e13a2346f28ad7866e19ecb9a9be6c1ea1a729f7
nanakomatasu <123913335+nanakomatasu@users.noreply.github.com>
2023-02-02 19:53:21 +0800

    完成了首页数据可视化

--- js/index.js
+++ js/index.js
@@ -9,80 +9,181 @@ async function getBack() {
     url: "/dashboard",
   });
   const arr = res.data.overview;
-  const arr1 = res.data.year;
-  console.log(arr1);
-  var myChart = echarts.init(document.getElementById("line"));
-  var option = {
-    title: {
-      text: "2022全学科薪资走势",
-      left: "2%",
-      top: "2%",
-    },
-    xAxis: {
-      type: "category",
-      data: arr1.map((item) => {
-        return item.month;
-      }),
-      axisLine: {
-        lineStyle: {
-          color: "#999",
-          type: "dashed",
-        },
+  const echartsArr = res.data.year;
+  console.log(res.data);
+  const groupArr = res.data.groupData;
+  console.log(groupArr);
+  function salaryEcharts(echartsArr) {
+    var myChart = echarts.init(document.getElementById("line"));
+    var option = {
+      title: {
+        text: "2022全学科薪资走势",
+        left: "2%",
+        top: "2%",
       },
-    },
-    yAxis: {
-      type: "value",
-      splitLine: {
-        lineStyle: {
-          type: "dashed",
-        },
-      },
-    },
-    tooltip: {
-      trigger: "axis",
-    },
-    grid: {
-      left: "15%",
-      top: "15%",
-      right: "15%",
-      bottom: "15%",
-    },
-    series: [
-      {
-        data: arr1.map((item) => {
-          return item.salary;
+      xAxis: {
+        type: "category",
+        data: echartsArr.map((item) => {
+          return item.month;
         }),
-        type: "line",
-        smooth: true,
-        symbolSize: "10",
-        lineStyle: {
-          width: 5,
-        },
-        areaStyle: {
-          color: {
-            type: "linear",
-            x: 0,
-            y: 0,
-            x2: 0,
-            y2: 1,
-            colorStops: [
-              {
-                offset: 0,
-                color: "#92c6ff", // 0% 处的颜色
-              },
-              {
-                offset: 0.6,
-                color: "#fff", // 100% 处的颜色
-              },
-            ],
-            global: false, // 缺省为 false
+        axisLine: {
+          lineStyle: {
+            color: "#999",
+            type: "dashed",
           },
         },
       },
-    ],
-    color: ["#62afff"],
-  };
-  myChart.setOption(option);
+      yAxis: {
+        type: "value",
+        splitLine: {
+          lineStyle: {
+            type: "dashed",
+          },
+        },
+      },
+      tooltip: {
+        trigger: "axis",
+      },
+      grid: {
+        left: "15%",
+        top: "15%",
+        right: "15%",
+        bottom: "15%",
+      },
+      series: [
+        {
+          data: echartsArr.map((item) => {
+            return item.salary;
+          }),
+          type: "line",
+          smooth: true,
+          symbolSize: "10",
+          lineStyle: {
+            width: 5,
+          },
+          areaStyle: {
+            color: {
+              type: "linear",
+              x: 0,
+              y: 0,
+              x2: 0,
+              y2: 1,
+              colorStops: [
+                {
+                  offset: 0,
+                  color: "#92c6ff", // 0% 处的颜色
+                },
+                {
+                  offset: 0.6,
+                  color: "#fff", // 100% 处的颜色
+                },
+              ],
+              global: false, // 缺省为 false
+            },
+          },
+        },
+      ],
+      color: ["#62afff"],
+    };
+    myChart.setOption(option);
+  }
+  salaryEcharts(echartsArr);
+  function salaryGroup(groupArr) {
+    var myChart = echarts.init(document.querySelector("#lines"));
+    var option1 = {
+      xAxis: {
+        type: "category",
+        data: groupArr[1].map((item) => {
+          return item.name;
+        }),
+        axisLine: {
+          lineStyle: {
+            color: "#c0bfc5",
+            type: "dashed",
+          },
+        },
+      },
+      yAxis: {
+        type: "value",
+        splitLine: {
+          show: true,
+          lineStyle: {
+            color: "#c0bfc5",
+            type: "dashed",
+          },
+        },
+      },
+      tooltip: {},
+      series: [
+        {
+          data: groupArr[1].map((item) => {
+            return item.hope_salary;
+          }),
+
+          type: "bar",
+        },
+        {
+          data: groupArr[1].map((item) => {
+            return item.salary;
+          }),
+          type: "bar",
+        },
+      ],
+      color: [
+        {
+          type: "linear",
+          x: 0,
+          y: 0,
+          x2: 0,
+          y2: 1,
+          colorStops: [
+            {
+              offset: 0,
+              color: "#4ba2f1", // 0% 处的颜色
+            },
+            {
+              offset: 1,
+              color: "#d4e6fc", // 100% 处的颜色
+            },
+          ],
+          global: false, // 缺省为 false
+        },
+        {
+          type: "linear",
+          x: 0,
+          y: 0,
+          x2: 0,
+          y2: 1,
+          colorStops: [
+            {
+              offset: 0,
+              color: "#52d8a9", // 0% 处的颜色
+            },
+            {
+              offset: 1,
+              color: "#d0f4e8", // 100% 处的颜色
+            },
+          ],
+          global: false, // 缺省为 false
+        },
+      ],
+    };
+    myChart.setOption(option1);
+    const btns = document.querySelectorAll("#btns button");
+    btns.forEach((ele) => {
+      ele.addEventListener("click", function () {
+        document.querySelector("#btns .btn-blue").classList.remove("btn-blue");
+        this.classList.add("btn-blue");
+        const index = this.innerHTML.trim();
+        option1.xAxis.data = groupArr[index].map((ele) => ele.name);
+        option1.series[0].data = groupArr[index].map((ele) => ele.hope_salary);
+        option1.series[1].data = groupArr[index].map((ele) => ele.salary);
+        myChart.setOption(option1);
+      });
+    });
+  }
+  salaryGroup(groupArr);
+
   for (const k in arr) {
     document.querySelector(`[name=${k}]`).innerHTML = arr[k];
   }
